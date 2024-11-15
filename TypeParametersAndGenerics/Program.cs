6c0b973f1127aac6dcdb9c177690f58b62c236fb
Tookey <rachel.tookey@gmail.com>
2024-11-15 10:33:02 +0000

    Creating generic  method Last List and a stack

--- TypeParametersAndGenerics/Program.cs
+++ TypeParametersAndGenerics/Program.cs
@@ -2,9 +2,39 @@
 {
     internal class Program
     {
+        public static T GetLastList<T>(T[] inputArray)
+        {
+            //return inputArray[inputArray.Length - 1];
+            return inputArray[^1];
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //int[] intArray = { 1, 5, 2, 8, 6 };
+            //Console.WriteLine(GetLastList(intArray)); // 6
+
+            //double[] doubleArray = { 10.5, 60.5, 2.2, 8.76, 6.1111 };
+            //Console.WriteLine(GetLastList(doubleArray)); // 6.1111
+
+            //string[] stringArray = { "hello", "world", "it's", "northcoders!" };
+            //Console.WriteLine(GetLastList(stringArray)); // "northcoders"
+
+            Stack<string> myStack = new Stack<string>();
+            myStack.Push("hello");
+            myStack.Push("world");
+            myStack.Push("today");
+
+            //myStack.Pop();
+            Console.WriteLine(myStack.Count);
+            foreach (string item in myStack)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+
         }
     }
 }
